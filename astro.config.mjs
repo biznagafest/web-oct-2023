4a015e13f9a5e3f8c5a91d4478d05450d332ee6d
David Rojo <davidrojo1997@gmail.com>
2023-08-05 13:56:31 +0200

    feat(analytics): add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-
+import vercel from "@astrojs/vercel/edge";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: "hybrid",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
